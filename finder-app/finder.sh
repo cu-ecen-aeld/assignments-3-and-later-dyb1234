50b2dd9a9e93eb45034d6e2cfde74c7eb44a910e
yubin <yubindong.umn@gmail.com>
2024-10-11 12:14:24 +0800

    finished assignment1

---
+++ finder-app/finder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -ne 2 ];
+then 
+    echo "paremeters are not specified"
+    exit 1
+fi
+if [ ! -d $1 ];
+then
+    echo "$1 does not represent a directory in a filesystem"
+    exit 1
+fi
+echo "$1"
+var1=$( find $1 -type f | wc -l )
+var2=$( find $1 -type f | xargs cat | grep $2 | wc -l)
+echo "The number of files are $var1 and the number of matching lines are $var2"
+
+
---
+++ finder-app/writer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $# -ne 2 ];
+then 
+    echo "paremeters are not specified"
+    exit 1
+fi
+mkdir -p $(dirname $1)
+touch $1
+echo $2 > $1
