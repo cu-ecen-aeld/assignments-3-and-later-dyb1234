7a98a84d22f3c146250db58f67e84224d12cc613
yubin <yubindong.umn@gmail.com>
2024-11-14 09:05:24 +0800

    finished assignment3 part2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+SYS_TOOLCHAINS=$(${CROSS_COMPILE}gcc -print-sysroot)
 
 if [ $# -lt 1 ]
 then
@@ -23,21 +24,29 @@ fi
 
 mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
+# cd "$OUTDIR"
+# if [ ! -d "${OUTDIR}/linux-stable" ]; then
+#     #Clone only if the repository does not exist.
+# 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+# 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+# fi
+# if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+#     cd linux-stable
+#     echo "Checking out version ${KERNEL_VERSION}"
+#     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
-fi
+#     # TODO: Add your kernel build steps here
+
+#     make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+#     make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+#     make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+#     make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+#     make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    
+# fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image $OUTDIR
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +57,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +70,51 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+cd "$OUTDIR/rootfs"
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp ${SYS_TOOLCHAINS}/lib/ld-linux-aarch64.so.1 lib/
+cp ${SYS_TOOLCHAINS}/lib64/libm.so.6 lib64/
+cp ${SYS_TOOLCHAINS}/lib64/libresolv.so.2 lib64/
+cp ${SYS_TOOLCHAINS}/lib64/libc.so.6 lib64/
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+${CROSS_COMPILE}gcc writer.c -o writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+# on the target rootfs
+cp finder.sh conf/username.txt conf/assignment.txt finder-test.sh autorun-qemu.sh "$OUTDIR"/rootfs/home
+cd "$OUTDIR"/rootfs/home
+mkdir conf
+cp "$FINDER_APP_DIR"/conf/username.txt "$FINDER_APP_DIR"/conf/assignment.txt "$OUTDIR"/rootfs/home/conf 
+cp  "$FINDER_APP_DIR"/writer ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+echo "Creating InitRamFs"
+cd "$OUTDIR"/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd "$OUTDIR"
+gzip -f initramfs.cpio
